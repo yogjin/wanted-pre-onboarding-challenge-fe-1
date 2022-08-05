de92369d53ddb515db2e6a9f1714d6f65b9f5b3a
yogjin <youngjin988@gmail.com>
2022-08-05 21:59:46 +0900

    feat: AuthPage Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
