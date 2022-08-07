d793d6ef8c0dbef49dfb05b6818452ce815e9990
yogjin <youngjin988@gmail.com>
2022-08-07 13:09:58 +0900

    feat: Add logout() in AuthService interface

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -3,4 +3,5 @@ import { AxiosResponse } from 'axios';
 export default interface AuthService {
   signUp(email: string, password: string): Promise<AxiosResponse>;
   login(email: string, password: string): Promise<AxiosResponse>;
+  logout(): void;
 }
