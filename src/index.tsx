a340c841e09d3afd62553cff205e3f63c91fdf8f
yogjin <youngjin988@gmail.com>
2022-08-07 14:49:39 +0900

    feat!(index): Add AuthProvider
    Use DI pattern

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { TokenStorageImpl } from './db/token';
+import { AuthServiceImpl } from './service/auth';
+import { AuthProvider } from './context/AuthContext';
+import HttpClientImpl from './network/http';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const baseURL = process.env.REACT_APP_BASE_URL || '';
+const httpClientImpl = new HttpClientImpl(baseURL);
+const tokenStorageImpl = new TokenStorageImpl('token');
+const authServiceImpl = new AuthServiceImpl(httpClientImpl, tokenStorageImpl);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider authService={authServiceImpl}>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
